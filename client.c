193073928fe8257588bc5f827380f1b609620b93
Jozef Maloch <Jozefmaloch@gmail.com>
2017-04-14 00:46:35 +0200

    second

--- client.c
+++ client.c
@@ -6,93 +6,65 @@
 #define NSTRS       3           /* no. of strings  */
 #define ADDRESS     "mysocket"  /* addr to connect */
 
-/*
- * Strings we send to the server.
- */
-char *strs[NSTRS] = {
-    "This is the first string from the client.\n",
-    "This is the second string from the client.\n",
-    "This is the third string from the client.\n"
-};
+
 
 int main(int argc, char *argv[])
 {
     char c;
     FILE *fp;
-    register int i, s, len, opt;
-    struct sockaddr_un saun;
+    register int i, sock, len, opt;
+    struct sockaddr_un address;
 
-    while((opt = getopt(argc, argv, "p")) != -1)
-        switch(opt)
+    while ((opt = getopt(argc, argv, "p")) != -1)
+        switch (opt)
         {
-            case 'p':
-                printf("Output: '%s': parameter prepinaca -p\n", argv[2]);
-                break;
-            default:
-                printf("Error: '%c': nesparvny prepinac", argv[0]);
-                exit(1);
+        case 'p':
+            printf("Output: '%s': parameter prepinaca -p\n", argv[2]);
+            break;
+        default:
+            printf("Error: '%c': nesparvny prepinac", argv[0]);
+            exit(1);
         }
 
-    /*
-     * Get a socket to work with.  This socket will
-     * be in the UNIX domain, and will be a
-     * stream socket.
-     */
-    if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
+
+    // initialize socket
+    if ((sock = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
         perror("client: socket");
         exit(1);
     }
 
     /*
-     * Create the address we will be connecting to.
-     */
-    saun.sun_family = AF_UNIX;
-    strcpy(saun.sun_path, ADDRESS);
+    * Create the address we will be connecting to.
+    */
+    address.sun_family = AF_UNIX;
+    strcpy(address.sun_path, ADDRESS);
+
+
+    len = sizeof(address.sun_family) + strlen(address.sun_path);
+
+    connect(sock, (struct sockaddr *)&address, len);
 
     /*
-     * Try to connect to the address.  For this to
-     * succeed, the server must already have bound
-     * this address, and must have issued a listen()
-     * request.
-     *
-     * The third argument indicates the "length" of
-     * the structure, not just the length of the
-     * socket name.
-     */
-    len = sizeof(saun.sun_family) + strlen(saun.sun_path);
+    * We'll use stdio for reading
+    * the socket.
+    */
 
-    connect(s, (struct sockaddr *)&saun, len); 
+    fp = fdopen(sock, "r");
 
-    /*
-     * We'll use stdio for reading
-     * the socket.
-     */
-    fp = fdopen(s, "r");
 
-    /*
-     * First we read some strings from the server
-     * and print them out.
-     */
-    for (i = 0; i < NSTRS; i++) {
+    // read & print string from the server
         while ((c = fgetc(fp)) != EOF) {
             putchar(c);
-
             if (c == '\n')
                 break;
         }
-    }
 
-    /*
-     * Now we send some strings to the server.
-     */
-    for (i = 0; i < strlen(argv[2]); i++)
-        send(s, argv[2], strlen(argv[2]), 0);
+    // send a string to the server
+    send(sock, argv[2], strlen(argv[2]), 0);
 
-    /*
-     * We can simply use close() to terminate the
-     * connection, since we're done with both sides.
-     */
-    close(s);
+
+    // close socket
+    close(sock);
 
     exit(0);
-}
+}   
